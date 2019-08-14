dc6c617d0d9329ea1d7cc51fe1c76344bfdf39bf
Connor Mason <connormason@apple.com>
2019-08-14 14:24:40 -0700

    Added missing RED color definition

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ WORK_DOTFILES_REPO="git@github.pie.apple.com:connor-mason/dotfiles.git"
 
 # Color definitions
 NC="\033[0m"
+RED="\033[0;31m"
 GREEN="\033[0;32m"
 CYAN="\033[0;36m"
 YELLOW="\033[1;33m"
