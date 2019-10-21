7ee83be78f126962ec20c7b06ca87682b5e40917
Connor Mason <connor@conmason.com>
2019-10-21 09:19:09 -0700

    Add step to upgrade pip

--- mac/install_mac.sh
+++ mac/install_mac.sh
@@ -46,6 +46,10 @@ echo -e "${CYAN}Installating python3...${NC}"
 brew install python3
 echo ""
 
+echo -e "${CYAN}Upgrading pip...${NC}"
+python3 -m pip install --upgrade pip
+echo ""
+
 echo -e "${CYAN}Installating ipython...${NC}"
 python3 -m pip install ipython --user
 echo ""
