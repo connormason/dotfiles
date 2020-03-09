7339e4ef22535b7b75562e83e14379ce5d7c26ed
Connor Mason <connor@conmason.com>
2020-03-08 19:47:45 -0700

    Added small script to determine environment for use in other scripts

---
+++ determine_environment.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
