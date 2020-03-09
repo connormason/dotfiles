14ab0d7db8806a5c27654d73cac4f1e406637fc7
Connor Mason <connor@conmason.com>
2020-03-09 00:45:55 -0700

    Add some terminal preferences to setup_preferences.sh

--- ubuntu/setup_preferences.sh
+++ ubuntu/setup_preferences.sh
@@ -23,5 +23,21 @@ gsettings set org.gnome.desktop.privacy remove-old-trash-files true
 gsettings set io.elementary.files.preferences show-hiddenfiles true
 gsettings set org.gtk.Settings.FileChooser show-hidden true
 
-# Disable unsafe paste alert in terminal (warns when pasting code with `sudo` in it)
+# Terminal
+## Disable unsafe paste alert in terminal (warns when pasting code with `sudo` in it)
 gsettings set io.elementary.terminal.settings unsafe-paste-alert false
+
+## Always show tabs in tab bar
+gsettings set io.elementary.terminal.settings tab-bar-behavior true
+
+## Prefer dark style
+gsettings set io.elementary.terminal.settings prefer-dark-style true
+
+## Natural copy and paste
+gsettings set io.elementary.terminal.settings natural-copy-paste true
+
+## Allow bold text
+gsettings set io.elementary.terminal.settings allow-bold true
+
+## Remember tabs on close
+gsettings set io.elementary.terminal.settings remember-tabs true
