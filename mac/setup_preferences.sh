b54c69a8163363d402ffcb1ba2d901b944524984
Connor Mason <connormason@apple.com>
2020-06-29 00:45:08 -0700

    Add app store mac prefs

--- mac/setup_preferences.sh
+++ mac/setup_preferences.sh
@@ -3,27 +3,6 @@
 NC="\033[0m"
 YELLOW="\033[0;33m"
 
-# ******************
-# App Store settings
-# ******************
-echo "Setting up App Store..."
-
-# Enable the automatic update check
-defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
-
-# Check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-# Download newly available updates in background
-defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
-
-# Install System data files & security updates
-defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
-
-# Turn on app auto-update
-defaults write com.apple.commerce AutoUpdate -bool true
-
-
 # **************
 # Other settings
 # **************
