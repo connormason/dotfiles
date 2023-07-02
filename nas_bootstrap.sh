b66a23e130c32157d5a7ebbbe1ab645bf296fdda
Connor Mason <connormason@apple.com>
2023-07-02 01:28:48 -0700

    Add role to setup tailscale

--- nas_bootstrap.sh
+++ nas_bootstrap.sh
@@ -3,5 +3,8 @@
 # Exit when any command fails
 set -e
 
+# Install Ansible requirements
+ansible-galaxy install -r roles/requirements.yml
+
 # Run NAS bootstrap Ansible playbook
 ansible-playbook playbooks/nas_bootstrap.yml -i inventory -v
